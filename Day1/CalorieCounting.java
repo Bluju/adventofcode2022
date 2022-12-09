3366f8c62cef1357584487eea363a67e35667feb
Bluju <JASahagun26@gmail.com>
2022-12-08 22:35:15 -0600

    Finished part 1 day 1

--- Day1/CalorieCounting.java
+++ Day1/CalorieCounting.java
@@ -1,39 +1,29 @@
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 public class CalorieCounting{
-
-    //public static int mostCalories()
     public static void main(String[] args){
-        int mostCalories = 0;
-        Scanner input = new Scanner(System.in);
-        System.out.println("Paste calorie list for elves: ");
-        
-        
-        int currentCalorie = 0;
+        int mostCalories = Integer.MIN_VALUE;
+        String line = "";
         int currentTotal = 0;
-        
-        while(input.hasNextInt()){
-            currentCalorie = input.nextInt();
-            System.out.println("CurrentCalorie is now: " + currentCalorie);
-            currentTotal += currentCalorie;
-            System.out.println("CurrentTotal is now: " + currentTotal);
-            if(currentTotal > mostCalories){
-                mostCalories = currentTotal;
-                System.out.println("MostCalories is now: " + mostCalories);
+        try{
+            File calFile = new File("ElfCalories.txt");
+            Scanner input = new Scanner(calFile);
+            while(input.hasNextLine()){
+                line = input.nextLine();
+                if(line.equals("")){
+                    currentTotal = 0;
+                    continue;
+                }
+                currentTotal += Integer.parseInt(line);
+                if(currentTotal > mostCalories){
+                    mostCalories = currentTotal;
+                }      
             }
-
-            if(input.findInLine("(?=\\S)") == null){
-                //input.nextLine();
-                currentTotal = 0;
-                System.out.println("currentTotal has been set to 0. MostCalories is still " + mostCalories + "\n");
-            }
-            
+            input.close();
+        }catch(FileNotFoundException e){
+            System.out.println("A file was not found");  
         }
-        input.close();
-        
-        System.out.println("The elf with the most calories has: " + mostCalories + " calories.");
-
-
+        System.out.println("The elf carrying food with the most calories has: " + mostCalories + " calories worth of food.");
     }
-
-
 }
